c6627bbb6083cdb3327ae92da31c93a484784bd1
chachmu <jbick3@gatech.edu>
2018-07-24 15:24:47 -0400

    Rename files to make more sense

--- guiController.py
+++ guiController.py
@@ -81,7 +81,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.43.54', 10000)
+server_address = ('localhost', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
