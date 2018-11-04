a478b15009f57d62af4740e61a859d909893a5ef
Hieu Nguyen <nthieu173@gmail.com>
2018-11-04 16:11:08 -0500

    Changed test according to vizier example 3

--- controller_test.py
+++ controller_test.py
@@ -1,56 +1,88 @@
-import vizier.node as vizier_node
+import argparse
 import json
+import vizier.node as vizier_node
 
-# Ensure that Node Descriptor File can be Opened
-node_descriptor = None
-try:
-        f = open("node_desc_controller.json", 'r')
-        node_descriptor = json.load(f)
-        print(node_descriptor)
-        f.close()
-except Exception as e:
-    print(repr(e))
-    print("Couldn't open given node file node_desc_controller.json")
+def main():
+    # Parse Command Line Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("node_descriptor", help=".json file node information",
+                         default="node_desc_controller.json")
+    parser.add_argument("-port", type=int, help="MQTT Port", default=8080)
+    parser.add_argument("-host", help="MQTT Host IP", default="localhost")
 
-node = vizier_node.Node("localhost", 1884, node_descriptor)
+    args = parser.parse_args()
 
-# Get the links for Publishing/Subscribing
-publishable_link = list(node.publishable_links)[0]
-subscribable_link = list(node.subscribable_links)[0]
-msg_queue = node.subscribe(subscribable_link)
-print("Got the links")
-
-#Initialize Connection
-print("Connecting to Robot")
-
-node.publish(publishable_link,"0,0,0")
-state = 0
-
-done = False
-recieved = False
-
-while not done:
-    # Recieve data here
+    # Ensure that Node Descriptor File can be Opened
+    node_descriptor = None
     try:
-        #Recieve Vizier Messages
-        message = msg_queue.get(timeout = 1).payload.decode(encoding='UTF-8')
-        state = int(message)
-    except:
-        if recieved:
-            node.publish(publishable_link,"0,0,0")
+        f = open(args.node_descriptor, 'r')
+        node_descriptor = json.load(f)
+        f.close()
+    except Exception as e:
+        print(repr(e))
+        print("Couldn't open given node file " + args.node_descriptor)
+        return -1
+
+    # Start the Node
+    node = vizier_node.Node(args.host, args.port, node_descriptor)
+    node.start()
+
+    # Get the links for Publishing/Subscribing
+    publishable_link = list(node.publishable_links)[0]
+    subscribable_link = list(node.subscribable_links)[0]
+    msg_queue = node.subscribe(subscribable_link)
+
+    # Control stuffs
+    ref = 5.0
+    state = 1.0e6
+    print('\n')
+    while abs(ref - state) >= 0.001:
+        try:
+            message = msg_queue.get(timeout=1).decode(encoding='UTF-8')
+            state = float(message)
+        except KeyboardInterrupt:
             break
-        else:
-            recieved = True
-            state = 1
+        except Exception:
+            continue
+        print('Control input = {}'.format(ref-state), end='\r')
+        node.publish(publishable_link, str(ref-state))
+    print('\n')
+    node.stop()
 
-    if state != 1:
-        #Robot sent back bad status
-        print("Connection to robot lost")
-        node.publish(publishable_link,"0,0,0")
-        break
 
-    #Send Vizier Message
-    node.publish(publishable_link, "important stuff")
+if(__name__ == "__main__"):
+main()
 
-print("Exiting")
+# #Initialize Connection
+# print("Connecting to Robot")
 
+# node.publish(publishable_link,"0,0,0")
+# state = 0
+
+# done = False
+# recieved = False
+
+# while not done:
+#     # Recieve data here
+#     try:
+#         #Recieve Vizier Messages
+#         message = msg_queue.get(timeout = 1).payload.decode(encoding='UTF-8')
+#         state = int(message)
+#     except:
+#         if recieved:
+#             node.publish(publishable_link,"0,0,0")
+#             break
+#         else:
+#             recieved = True
+#             state = 1
+
+#     if state != 1:
+#         #Robot sent back bad status
+#         print("Connection to robot lost")
+#         node.publish(publishable_link,"0,0,0")
+#         break
+
+#     #Send Vizier Message
+#     node.publish(publishable_link, "important stuff")
+
+# print("Exiting")
--- robot_test.py
+++ robot_test.py
@@ -1,51 +1,96 @@
+
+import time
 import json
+import argparse
+import random
 import vizier.node as vizier_node
 
-# Ensure that Node Descriptor File can be Opened
-node_descriptor = None
-try:
-        f = open("node_desc_robot.json", 'r')
+def main():
+
+    # Parse Command Line Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("node_descriptor", help=".json file node information",
+                        default="node_desc_robot.json")
+    parser.add_argument("-port", type=int, help="MQTT Port", default=8080)
+    parser.add_argument("-host", help="MQTT Host IP", default="localhost")
+
+    args = parser.parse_args()
+
+    # Ensure that Node Descriptor File can be Opened
+    node_descriptor = None
+    try:
+        f = open(args.node_descriptor, 'r')
         node_descriptor = json.load(f)
         f.close()
-except Exception as e:
-    print(repr(e))
-    print("Couldn't open given node file node_desc_robot.json")
+    except Exception as e:
+        print(repr(e))
+        print("Couldn't open given node file " + args.node_descriptor)
+        return -1
 
-# Start the Node
-node = vizier_node.Node("128.61.72.236", 1884, node_descriptor)
-node.start()
+    # Start the Node
+    node = vizier_node.Node(args.host, args.port, node_descriptor)
+    node.start()
 
+    # Get the links for Publishing/Subscribing
+    publishable_link = list(node.publishable_links)[0]
+    subscribable_link = list(node.subscribable_links)[0]
+    msg_queue = node.subscribe(subscribable_link)
 
-# Get the links for Publishing/Subscribing
-publishable_link = list(node.publishable_links)[0]
-subscribable_link = list(node.subscribable_links)[0]
-msg_queue = node.subscribe(subscribable_link)
-
-#Initialize Connection
-print("Connecting to Controller")
-
-node.publish(publishable_link,"1")
-message = msg_queue.get(timeout = 10).decode(encoding='UTF-8')
-
-recieved = False
-
-while True:
-    try:
-        # Recieve Vizier Messages
-        message = msg_queue.get(timeout = 1).decode(encoding='UTF-8')
-    except KeyboardInterrupt:
-        break
-    except:
-        if recieved:
-            print("Connection to Controller Lost")
-            node.publish(publishable_link, "0")
+    # Set the initial condition
+    state = 10*random.random() - 5
+    dt = 0.0
+    node.publish(publishable_link, str(state))
+    print('\n')
+    while abs(state - 5) >= 0.001:
+        tick = time.time()
+        try:
+            message = msg_queue.get(timeout=0.1).decode(encoding='UTF-8')
+            input = float(message)
+        except KeyboardInterrupt:
             break
-        else:
-            recieved = True
+        except Exception:
+            input = 0.0
+        tock = time.time()
+        dt = tock - tick
+        state = state + dt * input
+        print('\t\t\t\t\t\tState = {}'.format(state), end='\r')
+        node.publish(publishable_link, str(state))
+    print('\n')
+    node.stop()
 
-    # Send Vizier Message
-    node.publish(publishable_link,"1")
 
-# Clean up the connection
-node.publish(publishable_link,"0")
-node.stop()
+if (__name__ == "__main__"):
+    main()
+
+# # Start the Node
+# node = vizier_node.Node("128.61.72.236", 1884, node_descriptor)
+# node.start()
+
+# #Initialize Connection
+# print("Connecting to Controller")
+
+# node.publish(publishable_link,"1")
+# message = msg_queue.get(timeout = 10).decode(encoding='UTF-8')
+
+# recieved = False
+
+# while True:
+#     try:
+#         # Recieve Vizier Messages
+#         message = msg_queue.get(timeout = 1).decode(encoding='UTF-8')
+#     except KeyboardInterrupt:
+#         break
+#     except:
+#         if recieved:
+#             print("Connection to Controller Lost")
+#             node.publish(publishable_link, "0")
+#             break
+#         else:
+#             recieved = True
+
+#     # Send Vizier Message
+#     node.publish(publishable_link,"1")
+
+# # Clean up the connection
+# node.publish(publishable_link,"0")
+# node.stop()
