1a99da7352337854f0757ab3bdd6fb91e00b74d3
Hieu Nguyen <nthieu173@gmail.com>
2020-01-16 23:03:23 -0500

    Added new simpler controller and robot py

---
+++ controller_new.py
@@ -0,0 +1,94 @@
+import json
+import pyGui
+import socket
+import subprocess
+import time
+from vizier.node import Node
+
+class Controller:
+    def _init_(self, started_node: node.Node):
+        self.node = started_node
+
+        # Get the links for Publishing/Subscribing
+        self.publishable_link = list(started_node.publishable_links)[0]
+        self.subscribable_link = list(started_node.subscribable_links)[0]
+        self.msg_queue = self.node.subscribe(self.subscribable_link)
+    
+    def send(self, message: str):
+        self.node.publish(self.publishable_link, message)
+    
+    def receive(self, block=True, timeout=None):
+        return self.msg_queue.get(block=block, timeout=timeout).decode(encoding = 'UTF-8')
+    
+def start_mosquitto(port: int):
+    command = ["mosquitto", "-p", str(port)]
+    mosquitto = subprocess.Popen(command)
+    time.sleep(0.5)
+    return mosquitto
+
+def stop_mosquitto(process: Popen):
+    process.terminate()
+    time.sleep(0.5)
+
+def get_host_IP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #the IP address doesn't have to be reachable
+    s.connect(("10.255.255.255", 1)) 
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+if __name__ == "__main__":
+    # Default values
+    HOST_PORT = 8080
+    DESC_FILENAME = "./node_desc_controller.json"
+    ALL_MODES = ["joystick", "keyboard", "auto"]
+    MODE = ALL_MODES[0]
+
+    main(HOST_PORT, DESC_FILENAME, MODE)
+
+def main(port: int, desc_filename: str, mode: str):
+    node_desc = None
+    with open(desc_filename, 'r') as desc_file:
+        node_desc = json.load(desc_file)
+    
+    mosquitto = start_mosquitto(HOST_PORT)
+    host_node = Node(get_host_IP(), HOST_PORT, node_desc)
+    host_node.start()
+    controller = Controller(host_node)
+
+    if MODE == "joystick" or MODE == "keyboard":
+        gui = pyGui.Gui(hasJoystick = MODE == "joystick")
+
+        state = {"alive": True}
+        while state["alive"]:
+            gui.render()
+            try:
+                state = json.loads(controller.receive(timeout=0.1))
+            except:
+                continue
+            if (MODE == "joystick"):
+                command = gui.get_joystick_axis()
+            elif (MODE == "keyboard"):
+                command = gui.get_keyboard_command()
+            state["command"] = (pwm(-command[0]), pwm(-command[1]), pwm(-command[2]), pwm(-command[3]))
+            controller.send(json.dumps(state, separators=(',',':')))
+
+            def pwm(value):
+                max_pwm = 1832
+                min_pwm = 1148
+                center = (max_pwm + min_pwm)/2
+                diff = (max_pwm - min_pwm)/2
+                return int(center + (diff * value))
+    elif MODE == "auto":
+        state = {"alive": True}
+        while state["alive"]:
+            try:
+                state = json.loads(controller.receive(timeout=0.1))
+            except:
+                continue
+            state["command"] = {"lat":1,"lon":1,"alt":1}
+            controller.send(json.dumps(state, separators=(',',':')))
+
+    host_node.stop()
+    stop_mosquitto()
---
+++ robot_new.py
@@ -0,0 +1,189 @@
+import dronekit
+import json
+import socket
+import time
+from vizier.node import Node
+
+GPS_FIX_TYPE = {
+    "no_gps": 0,
+    "no_fix": 1,
+    "2d_fix": 2,
+    "3d_fix": 3,
+    "dgps": 4,
+    "rtk_float": 5,
+    "rtk_fixed": 6,
+    "static": 7,
+    "ppp": 8
+}
+GPS_INPUT_IGNORE_FLAGS = {
+    "alt": 1,
+    "hdop": 2,
+    "vdop": 4,
+    "vel_horiz": 8,
+    "vel_vert": 16,
+    "speed_accuracy": 32,
+    "horizontal_accuracy": 64,
+    "vertical_accuracy": 128
+}
+
+class Drone:
+    def __init__(self, started_node: Node, connection_string: str, vehicle_mode: str):
+        self.node = started_node
+
+        # Get the links for Publishing/Subscribing
+        self.publishable_link = list(started_node.publishable_links)[0]
+        self.subscribable_link = list(started_node.subscribable_links)[0]
+        self.msg_queue = self.node.subscribe(self.subscribable_link)
+ 
+        print("Connecting")
+        connected = False
+        while not connected:
+            try:
+                self.vehicle = dronekit.connect(connection_string, wait_ready=True)
+                self.vehicle.wait_ready(True)
+                connected = True
+            except Exception as e:
+                print("Error:", e, "retrying")
+                time.sleep(1)
+        print("Connected")
+
+        self.vehicle.mode = dronekit.VehicleMode(vehicle_mode)
+        print("Setting mode:", vehicle_mode, end="")
+        while self.vehicle.mode.name != vehicle_mode:
+            self.vehicle.mode = dronekit.VehicleMode(vehicle_mode)
+            print(".",end="")
+            time.sleep(1)
+        print("\nMode set:", self.vehicle.mode.name)   
+
+    def send(self, message: str):
+        self.node.publish(self.publishable_link, message)
+    
+    def receive(self, block=True, timeout=None):
+        return self.msg_queue.get(block=block, timeout=timeout).decode(encoding = 'UTF-8')
+    
+    def arm_vehicle(self):
+        self.vehicle.armed = True
+        print("Arming", end="")
+        while not self.vehicle.armed:
+            self.vehicle.armed = True
+            print(".",end="")
+            time.sleep(1)
+        print("\nArmed")
+    
+    def disarm_vehicle(self):
+        self.vehicle.armed = False
+        print("Disarming", end="")
+        while self.vehicle.armed:
+            self.vehicle.armed = False
+            print(".",end="")
+            time.sleep(1)
+        print("\nDisarmed")
+    
+class GuidedDrone(Drone):
+    def __init__(self, connection_string: str):
+        super().__init__(connection_string, "GUIDED")
+        guided_parameters = {
+            "EK3_ENABLE": 1,
+            "EK2_ENABLE": 0,
+            "AHRS_EKF_TYPE": 3,
+            "EK3_GPS_TYPE": 0,
+            "EK3_MAG_CAL": 5,
+            "EK3_ALT_SOURCE": 2,
+            "GPS_TYPE": 14,
+            "GPS_DELAY_MS": 50,
+            "COMPASS_USE": 0,
+            "COMPASS_USE2": 0,
+            "COMPASS_USE3": 0
+        }
+        for key in guided_parameters:
+            self.vehicle.parameters[key] = guided_parameters[key]
+
+    def command(self, lat: float, lon: float, alt = 0):
+        self.vehicle.simple_goto(dronekit.LocationGlobal(lat, lon, alt))
+
+    def send_GPS(self, lat: float, lon: float, alt: float, satellites_visible = 3):
+        ignore_flags = 0
+        for flag  in ["vel_horiz", "vel_vert", "speed_accuracy", "horizontal_accuracy"]:
+            ignore_flags = ignore_flags | GPS_INPUT_IGNORE_FLAGS[flag]
+        msg = self.vehicle.message_factory.gps_input_encode(
+            int(time.time()*10000), #Timestamp (micros since boot or Unix epoch)
+            0,                      #ID of the GPS for multiple GPS inputs
+            ignore_flags,                 #Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
+            0,                      #GPS time (milliseconds from start of GPS week)
+            0,                      #GPS week number
+            GPS_FIX_TYPE["3d_fix"], #0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
+            int(lat * (10**7)),     #Latitude (WGS84), in degrees * 1E7
+            int(lon * (10**7)),     #Longitude (WGS84), in degrees * 1E7
+            alt,                    #Altitude (AMSL, not WGS84), in m (positive for up)
+            0,                      #GPS HDOP horizontal dilution of position in m
+            0,                      #GPS VDOP vertical dilution of position in m
+            0,                      #GPS velocity in m/s in NORTH direction in earth-fixed NED frame
+            0,                      #GPS velocity in m/s in EAST direction in earth-fixed NED frame
+            0,                      #GPS velocity in m/s in DOWN direction in earth-fixed NED frame
+            0,                      #GPS speed accuracy in m/s
+            0,                      #GPS horizontal accuracy in m
+            0,                      #GPS vertical accuracy in m
+            satellites_visible      #Number of satellites visible.
+        )
+        self.vehicle.send_mavlink(msg)
+
+class ManualDrone(Drone):
+    def __init__(self, connection_string: str):
+        super().__init__(connection_string, "MANUAL")
+
+    def command(self, pitch: int, roll: int, yaw: int, throttle: int):
+        # Ch1 =Roll, Ch 2=Pitch, Ch 3=Horizontal throttle, Ch 4=Yaw, Ch 5=Forward throttle
+        self.vehicle.channels.overrides['1'] = roll
+        self.vehicle.channels.overrides['2'] = pitch
+        self.vehicle.channels.overrides['5'] = throttle
+        self.vehicle.channels.overrides['4'] = yaw
+
+def host_ready(ip: str, port: int): 
+    try:
+        prober = socket.create_connection((ip, port))
+        prober.close()
+        return True
+    except Exception as e:
+        return False
+
+if __name__ == "__main__":
+    # Parse Command Line Arguments
+    HOST_IP = 'localhost'
+    PORT = 8080
+    DESC_FILENAME = './node_desc_robot.json'
+    DEVICE_ID = 'usb-ArduPilot_fmuv2_25003C000E51373339363131-if00'
+    ALL_MODES = ['MANUAL', 'GUIDED']
+    MODE = ALL_MODES[0]
+
+    main(HOST_IP, PORT, DEVICE_ID, MODE)
+
+def main(host_ip: str, port: int, desc_filename: str, connection_string: str, mode: str):
+    node_desc = None
+    with open(desc_filename, 'r') as desc_file:
+        node_desc = json.load(desc_file)
+    node = Node(host_ip, port, node_desc)
+
+    while not host_ready(host_ip, port):
+        time.sleep(1)
+        continue
+
+    node.start()
+
+    drone = None
+    if mode == "MANUAL":
+        drone = ManualDrone(connection_string)
+    elif mode == "GUIDED":
+        drone = GuidedDrone(connection_string)
+    else:
+        drone = Drone(connection_string, mode)
+
+    state = {"alive": True}
+    while state["alive"]:
+        # Send the initial condition to the PC
+        drone.send(json.dumps(state, separators = (',', ':')))
+        try:
+            message = drone.receive(timeout=0.1).decode(encoding="UTF-8")
+        except:
+            continue
+        state = json.loads(message)
+        drone.command(*state)
