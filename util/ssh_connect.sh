998f1ff28bc8bf1587749a7247feb53e32299354
tate <taintertot514@gmail.com>
2020-10-15 16:23:57 -0400

    Changed ssh stuff

--- util/ssh_connect.sh
+++ util/ssh_connect.sh
@@ -1,1 +1,1 @@
-ssh pi@$(sudo arp-scan --local | grep b8:27:eb:b9:7f:3a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+ssh pi@$(sudo arp-scan --local | grep b8:27:eb:20:36:a0 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
