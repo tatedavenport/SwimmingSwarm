2aebc44f334ddc51f8dd444206c607eaac8b5658
Hieu Nguyen <nthieu173@gmail.com>
2021-01-15 11:32:50 -0500

    Overhaul the Overlord and Drone class

--- swarm/drone.py
+++ swarm/drone.py
@@ -1,233 +1,181 @@
+"""
+Simplify connection to Pixhawk and communication though the MQTT broker
+"""
+
+import logging
 import json
 import time
 import queue
-import socket
-import vizier.node
-from dronekit import connect, VehicleMode, LocationGlobal
+from typing import Dict
 
-from swarm.gps import MockGPS
+from vizier import node
+from dronekit import connect, VehicleMode
+
 
 class Drone:
-    def __init__(self, connection_string: str, host: str, port: int,
-                 node_descriptor: dict, vehicle_mode: str,
-                 local: bool = False, verbose: bool = False):
-        self._verbose = verbose
-        self._local = local
-        self._event = {
-            "armed": [], "connected":[], "disconnected":[],
-            "disarmed": [], "message": [], "loop": []
-            }
-        if not local:
-            if self._verbose: print("Setting up Vizier node...")
-            self.node = vizier.node.Node(host, port, node_descriptor)
-        self._init_vehicle(connection_string)
-        guided_parameters = {
-            "EK3_ENABLE": 1,
-            "EK2_ENABLE": 0,
-            "AHRS_EKF_TYPE": 3,
-            "EK3_GPS_TYPE": 0,
-            "EK3_MAG_CAL": 5,
-            "EK3_ALT_SOURCE": 2,
-            "GPS_TYPE": 14,
-            "GPS_DELAY_MS": 50,
-            "COMPASS_USE": 0,
-            "COMPASS_USE2": 0,
-            "COMPASS_USE3": 0
-        }
-        if vehicle_mode == "GUIDED":
-            for key in guided_parameters:
-                self.vehicle.parameters[key] = guided_parameters[key]
-        if self._verbose: print("Parameters", self.vehicle.parameters)
-        self._vehicle_mode = vehicle_mode
-        self.started = False
+    """
+    To use this class, you must override handle_start, handle_message and/or handle_stop.
+    Then, create this class either through new() or from_config().
+    DO NOT use __init__().
+    Finally, run start(). stop() must be arranged to
+    be called else start() will block forever
+    """
 
-        # TODO: Add actual GPS
-        self.gps = MockGPS(0, 0)
-    
+    @classmethod
+    def from_config(cls, path: str):
+        """
+        Create new Drone from a configuration file.
+        """
+        with open(path, "r") as file:
+            configuration = json.load(file)
+            connection_string = configuration["connection_string"]
+            vehicle_mode = configuration["vehicle_mode"]
+            broker_ip = configuration["broker"]["ip"]
+            broker_port = configuration["broker"]["port"]
+            node_descriptor = configuration["node"]
+            return cls.new(
+                connection_string, vehicle_mode, broker_ip, broker_port, node_descriptor
+            )
+
+    @classmethod
+    def new(
+        cls,
+        connection_string: str,
+        vehicle_mode: str,
+        broker_ip: str,
+        broker_port: int,
+        node_descriptor: Dict,
+    ):
+        """
+        Create new Overlord from connection_string, vehicle_mode,
+        broker ip, broker port and a node descriptor dict
+        """
+        drone = Drone()
+        drone.drone_node = node.Node(broker_ip, broker_port, node_descriptor)
+        drone.vehicle = connect(connection_string, wait_ready=True, baud=57600)
+        drone.set_vehicle_mode(vehicle_mode)
+
+    def __init__(self):
+        self.drone_node = None
+        self.vehicle = None
+        self.active = False
+        self.subscribables = {}
+
     def start(self):
-        self.started = True
-        self.set_vehicle_mode(self._vehicle_mode)
-        self.arm_vehicle()
-        self._message_loop()
-    
-    def stop(self):
-        if self.started:
-            self.started = False
-            if not self._local:
-                self.node.stop()
-                self._fire_event("disconnected")
-            self.disarm_vehicle()
-        elif self._verbose: print("Vehicle not started")
-    
-    def _message_loop(self):
         """
-        Start listening and responding to MQTT commands
+        Arm vehicle and start listening and responding to MQTT commands
         """
-        if not self._local:
-            self._start_node()
-            publishable_link = list(self.node.publishable_links)[0]
-            subscribable_link = list(self.node.subscribable_links)[0]
-            msg_queue = self.node.subscribe(subscribable_link)
-            self._fire_event("connected")
-            if self._verbose: print("Subscribed to broker")
-        
-            state = {"alive": True}
-            if self.vehicle.mode.name == "GUIDED": state["gps"] = self.gps.encoded_coord()
+        # Wait for vehicle armable
+        self.wait_vehicle_armable()
+        # Arm verhicle
+        self.set_vehicle_armed(True)
+        # Start Vizier node
+        self.drone_node.start()
+        # Trigger overriden function after vehicle had been armed and Vizier node set up
+        self.handle_start()
+        # Subscribe to all links
+        for link in self.drone_node.subscribable_links:
+            self.subscribables[link] = self.drone_node.subscribe(link)
 
-            # Send the initial condition to the PC
-            self.node.publish(publishable_link, json.dumps(state, separators = (',', ':')))
-            if self._verbose: print("Sent initial state")
-            while state["alive"] == True:
+        # Send the initial condition
+        state = {"alive": True}
+
+        for link in self.drone_node.publishable_links:
+            self.drone_node.publish(link, json.dumps(state, separators=(",", ":")))
+
+        while self.active:
+            for link in self.subscribables:
+                sub_queue = self.subscribables[link]
                 try:
-                    # Receive and decode the message
-                    message = msg_queue.get(timeout=0.1).decode(encoding = 'UTF-8')
-                    if self._verbose: print(message)
-                    # Fire any commands
-                    self._fire_event("message", self, message)
+                    msg = sub_queue.get(block=False).decode(encoding="UTF-8")
+                    self.handle_message(link, msg)
                 except queue.Empty:
-                    # Just pass if the queue is empty (no message received yet)
                     pass
-                except KeyboardInterrupt:
-                    # Stop when there's keyboard interrupt on the PI
-                    state["alive"] =  False
-                except Exception as e:
-                    if self._verbose: print(e)
-                    # Stop when there's other exception
-                    state["alive"] =  False
-                finally:
 
-                    if self.vehicle.mode.name == "GUIDED":
-                        # TODO: Remove for actual GPS. Step the mock gps simlation.
-                        self.gps.step(0.01)
-                        # Update the state
-                        state["gps"] = self.gps.encoded_coord()
-                        # Send updated GPS
-                        self.send_GPS(self.gps.lat, self.gps.lon, alt=0)
+        # Disarm verhicle
+        self.set_vehicle_armed(False)
+        self.handle_stop()
+        self.drone_node.stop()
 
-                    self._fire_event("loop", self)
-                    # Always send the updated state to the PC
-                    self.node.publish(publishable_link, json.dumps(state, separators = (',', ':')))
-        else:
-            state = 1
-            while state == 1:
-                try:
-                    self._fire_event("message", self, "")
-                except KeyboardInterrupt:
-                    # Stop when there's keyboard interrupt on the PI
-                    state = 0
-                except Exception as e:
-                    if self._verbose: print(e)
-                    state = 0
+        # Disarm vehicle
+        self.set_vehicle_armed(False)
 
-        self.stop()
+    def handle_start(self):
+        """
+        You must implement this method to handle what happens
+        AFTER the MQTT node is started and the vehicle is armed
+        but BEFORE the event loop is started.
+        """
+        # pylint: disable=no-self-use
+        return
+
+    def handle_message(self, link: str, msg: str):
+        """
+        You must implement this method to handle messages sent by the controller.
+        """
+        # pylint: disable=unused-argument, no-self-use
+        return
+
+    def handle_stop(self):
+        """
+        You must implement this method to handle what happens
+        AFTER the event loop is stopped and the vehicle is disarmed
+        but BEFORE the MQTT node is stopped.
+        """
+        # pylint: disable=no-self-use
+        return
+
+    def stop(self):
+        """
+        Stop event loop for the MQTT client, then disarm the vehicle
+        """
+        self.active = False
 
     def set_vehicle_mode(self, new_mode: str):
-        if self._verbose: print("Switching mode from", self.vehicle.mode.name, "to", new_mode)
+        """
+        Set the vehicle mode. Will block until mode changed successfully.
+        """
+        logging.info("Set vehice mode: %s", new_mode)
         self.vehicle.mode = VehicleMode(new_mode)
         while self.vehicle.mode.name != new_mode:
-            if self._verbose: print("Waiting for mode change...")
-            if new_mode == "GUIDED":
-                self.send_GPS(self.gps.lat, self.gps.lon, alt=0)
-            time.sleep(1)
-        if self._verbose: print("Successfully set mode ", self.vehicle.mode.name)
-
-    def add_event_listener(self, event_name: str, listener: callable):
-        self._event[event_name].append(listener)
- 
-    def remove_event_listeners(self, event_name: str):
-        self._event[event_name] = []
-    
-    def _fire_event(self, event_name: str, *args):
-        for listener in self._event[event_name]:
-            listener(*args)
-                
-    def _start_node(self):
-        if self._verbose: print("Starting Vizier")
-        connected = False
-        while not connected:
-            try:
-                # Probing broker
-                prober = socket.create_connection((self.node._host, self.node._port))
-                connected = True
-                prober.close()
-            except Exception as e:
-                print("Retrying:", e)
-                time.sleep(1)
-        self.node.start()
-        if self._verbose: print("Connected to Vizier")
-
-    def _init_vehicle(self, connection_string: str):
-        if self._verbose: print("Connecting to vehicle")
-        connected = False
-        while not connected:
-            try:
-                self.vehicle = connect(connection_string, wait_ready=True)
-                self.vehicle.wait_ready(True)
-                connected = True
-            except Exception as e:
-                if self._verbose:
-                    print("Retrying:", e)
             time.sleep(1)
 
-    def arm_vehicle(self):
-        if self._verbose: print("Arming vehicle")
-        self.vehicle.armed = True
+    def set_vehicle_armed(self, armed: bool):
+        """
+        Arm the vehicle. Will block until vehicle is armed.
+        """
+        logging.info("Arming motors" if armed else "Disarming motors")
+        self.vehicle.armed = armed
         while not self.vehicle.armed:
-            if self._verbose: print("Waiting for arming...")
             time.sleep(1)
-        self._fire_event("armed")
-        if self._verbose: print("Vehicle armed")
-    
-    def disarm_vehicle(self):
-        if self._verbose: print("Disarming vehicle")
-        self.vehicle.armed = False
-        while self.vehicle.armed:
-            if self._verbose: print("Waiting for disarm...")
-            self.vehicle.armed = False
+
+    def wait_vehicle_armable(self):
+        """
+        Block until the vehicle is armable.
+        """
+        logging.info("Basic pre-arm checks")
+        while not self.vehicle.is_armable:
             time.sleep(1)
-        if self._verbose: print("Vehicle disarmed")
+
+    def publish(self, link: str, message: str):
+        """
+        Publish a message through this link
+        """
+        self.drone_node.publish(link, message)
+
+    def publish_all(self, message: str):
+        """
+        Publish a message through all links connected to this node
+        """
+        for link in self.drone_node.publishable_links:
+            self.drone_node.publish(link, message)
 
     def channel_command(self, pitch: int, roll: int, yaw: int, throttle: int):
-        if self._verbose: print(roll, pitch, throttle, yaw)
+        """
+        Directly control the vehicle through motor channels
+        """
         # Ch1 =Roll, Ch 2=Pitch, Ch 3=Horizontal throttle, Ch 4=Yaw, Ch 5=Forward throttle
-        self.vehicle.channels.overrides['1'] = roll
-        self.vehicle.channels.overrides['2'] = pitch
-        self.vehicle.channels.overrides['5'] = throttle
-        self.vehicle.channels.overrides['4'] = yaw
-
-    def guided_command(self, lat: float, lon: float, alt = 0):
-        if self._verbose: print(lat, lon, alt)
-        self.vehicle.simple_goto(LocationGlobal(lat, lon, alt))
-    
-    def send_GPS(self, lat: float, lon: float, alt: float, satellites_visible = 3):
-        gps_fix_type = {
-            "no_fix": 0, "2d_fix": 2,
-            "3d_fix": 3, "dgps": 4, "rtk_float": 5
-        }
-        ignore = {
-            "alt": 1, "hdop": 2, "vdop": 4, "vel_horiz": 8, "vel_vert": 16,
-            "speed_accuracy": 32, "horizontal_accuracy": 64, "vertical_accuracy": 128
-        }
-        ignore_flags = ignore["vel_horiz"] | ignore["vel_vert"]
-        ignore_flags = ignore_flags | ignore["speed_accuracy"] | ignore["horizontal_accuracy"]
-        msg = self.vehicle.channels(
-            int(time.time()*10000), #Timestamp (micros since boot or Unix epoch)
-            0,                      #ID of the GPS for multiple GPS inputs
-            ignore,                 #Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
-            0,                      #GPS time (milliseconds from start of GPS week)
-            0,                      #GPS week number
-            gps_fix_type["3d_fix"], #0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
-            int(lat * (10**7)),     #Latitude (WGS84), in degrees * 1E7
-            int(lon * (10**7)),     #Longitude (WGS84), in degrees * 1E7
-            alt,                    #Altitude (AMSL, not WGS84), in m (positive for up)
-            0,                      #GPS HDOP horizontal dilution of position in m
-            0,                      #GPS VDOP vertical dilution of position in m
-            0,                      #GPS velocity in m/s in NORTH direction in earth-fixed NED frame
-            0,                      #GPS velocity in m/s in EAST direction in earth-fixed NED frame
-            0,                      #GPS velocity in m/s in DOWN direction in earth-fixed NED frame
-            0,                      #GPS speed accuracy in m/s
-            0,                      #GPS horizontal accuracy in m
-            0,                      #GPS vertical accuracy in m
-            satellites_visible      #Number of satellites visible.
-        )
-        self.vehicle.send_mavlink(msg)
+        self.vehicle.channels.overrides["1"] = roll
+        self.vehicle.channels.overrides["2"] = pitch
+        self.vehicle.channels.overrides["5"] = throttle
+        self.vehicle.channels.overrides["4"] = yaw
--- swarm/overlord.py
+++ swarm/overlord.py
@@ -1,172 +1,112 @@
-import subprocess
-import time
-import socket
-import paramiko
-import queue
+"""
+Simplify connection and communication through an MQTT broker
+"""
+
 import json
-import threading
-import hashlib
-from collections import namedtuple
+import queue
+from typing import Dict
+
 from vizier import node
 
-def update(bot, bot_ssh: paramiko.SSHClient):
-    def always(bot, bot_ssh: paramiko.SSHClient):
-        files_to_update = []
-        if not md5sum_equal(bot.config["source"], bot.config["dest"], bot_ssh):
-            files_to_update.append((bot.config["source"], bot.config["dest"]))
-        if not md5sum_equal(bot.script["source"], bot.script["dest"], bot_ssh):
-            files_to_update.append((bot.script["source"], bot.script["dest"]))
-        upload_files(bot_ssh, files_to_update)
-    
-    def never(bot, bot_ssh):
-        pass
-
-    execute_update = {"always": always, "never": never}
-    return execute_update[bot.update_mode](bot, bot_ssh)
-
-def md5sum_equal(source, dest, bot_ssh):
-    std_in, stdout, stderr = bot_ssh.exec_command("md5sum " + dest + "; echo $?")
-    md5out = stdout.readline()
-    md5out_code = int(stdout.readline().strip())
-    if md5out_code == 0:
-        md5out = md5out.split()[0]
-        with open(source, "rb") as file:
-            return hashlib.md5(file.read()).hexdigest() == md5out
-    else:
-        return False
-
-def connect_by_SSH(ip: str, uname: str, passw: str):
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(ip, username = uname, password = passw)
-    return ssh_client
-
-def upload_files(ssh_client: paramiko.SSHClient, paths: list):
-    ftp_client=ssh_client.open_sftp()
-    for source, dest in paths:
-        ftp_client.put(source, dest)
-    ftp_client.close()
-
-def _read_ssh_ouput(stdout, stderr, output):
-    if output.lower() == "stdout":
-        return stdout.read()
-    elif output.lower() == "stderr":
-        return stderr.read()
-    elif output.lower() == "both":
-        return stdout, stdout
-    else:
-        return None
 
 class Overlord:
-    def __init__(self, config: object, verbose: bool = False):
-        self.host = namedtuple("Host", config["host"].keys())(*config["host"].values())
-        self.bots = []
-        for bot in config["bots"]:
-            self.bots.append(namedtuple("Bot", bot.keys())(*bot.values()))
-        self.host_ip = self._get_host_IP()
-        print("Host IP:", self.host_ip)
-        self.host_node = node.Node(self.host_ip, self.host.port, self.host.node)
-        self.started = False
-        self._event = {"start": [], "loop": [], "stop":[]}
-        self._bot_ip = None
-        self._verbose = verbose
-    
+    """
+    To use this class, you must override handle_start, handle_message and/or handle_stop.
+    Then, create this class either through new() or from_config().
+    DO NOT use __init__().
+    Finally, run start(). stop() must be arranged to
+    be called else start() will block forever
+    """
+
+    def __init__(self):
+        self.host_node = None
+        self.active = False
+        self.subscribables = {}
+
+    @classmethod
+    def from_config(cls, path: str):
+        """
+        Create new Overlord from a configuration file.
+        """
+        with open(path, "r") as file:
+            configuration = json.load(file)
+            broker_ip = configuration["broker"]["ip"]
+            broker_port = configuration["broker"]["port"]
+            node_descriptor = configuration["node"]
+            return cls.new(broker_ip, broker_port, node_descriptor)
+
+    @classmethod
+    def new(cls, broker_ip: str, broker_port: int, node_descriptor: Dict):
+        """
+        Create new Overlord from broker ip, broker port and a node descriptor dict
+        """
+        overlord = cls()
+        overlord.host_node = node.Node(broker_ip, broker_port, node_descriptor)
+        overlord.active = False
+        overlord.subscribables = {}
+        return overlord
+
+    def handle_start(self):
+        """
+        You must implement this method to handle what happens
+        AFTER the MQTT node is started but BEFORE the event loop is started.
+        """
+        # pylint: disable=no-self-use
+        return
+
+    def handle_message(self, link: str, msg: str):
+        """
+        You must implement this method to handle messages published by a robot.
+        """
+        # pylint: disable=unused-argument, no-self-use
+        return
+
+    def handle_stop(self):
+        """
+        You must implement this method to handle what happens
+        AFTER the event loop is stopped but BEFORE the MQTT node is stopped.
+        """
+        # pylint: disable=no-self-use
+        return
+
     def start(self):
-        try:
-            self._start_mosquitto()
-            self.host_node.start()
-            #bot_ssh = None
-            #for bot in self.bots:
-            #    bot_ssh = connect_by_SSH(self._get_bot_IP(bot.mac), bot.username, bot.password)
-            #    update(bot, bot_ssh)
+        """
+        Call this method to start the event loop for the MQTT client.
+        """
+        self.host_node.start()
+        self.handle_start()
 
-            # Get the links for Publishing/Subscribing
-            self.publishable_link = list(self.host_node.publishable_links)[0]
-            self.subscribable_link = list(self.host_node.subscribable_links)[0]
+        for link in self.host_node.subscribable_links:
+            self.subscribables[link] = self.host_node.subscribe(link)
 
-            self.msg_queue = self.host_node.subscribe(self.subscribable_link)
-            self.started = True
-            self._fire_event("start")
-            if self._verbose: print("Starting loop")
-            while self.started:
+        self.active = True
+        while self.active:
+            for link in self.subscribables:
+                sub_queue = self.subscribables[link]
                 try:
-                    self._fire_event("loop")
-                except KeyboardInterrupt:
-                    self.stop()
+                    msg = sub_queue.get(block=False).decode(encoding="UTF-8")
+                    self.handle_message(link, msg)
                 except queue.Empty:
-                    if self._verbose: print("Empty queue")
                     pass
-                except Exception as e:
-                    print(e)
-                    self.stop()
 
-        finally:
-            self.host_node.stop()
-            self._stop_mosquitto()
-        
+        self.handle_stop()
+        self.host_node.stop()
+
     def stop(self):
-        self._fire_event("stop")
-        self.started = False
-    
-    def publish(self, message):
-        self.host_node.publish(self.publishable_link, message)
-    
-    def get_message(self, block=True, timeout=None):
-        return self.msg_queue.get(block=block, timeout=timeout).decode(encoding = 'UTF-8')
+        """
+        Stop event loop for the MQTT client
+        """
+        self.active = False
 
-    def add_event_listener(self, event_name: str, listener: callable):
-        # All events expects nothing and return nothing. Control the object through public methods.
-        self._event[event_name].append(listener)
-    
-    def remove_event_listeners(self, event_name: str):
-        self._event[event_name] = []
-    
-    def _fire_event(self, event_name: str, *args):
-        data = []
-        for listener in self._event[event_name]:
-            data.append(listener(*args))
-        if len(data) == 0:
-            return None
-        elif len(data) == 1:
-            return data[0]
-        else:
-            return data
-    
-    def _start_mosquitto(self):
-        command = ["mosquitto", "-p", str(self.host.port)]
-        self.mosquitto = subprocess.Popen(command)
-        time.sleep(1)
-    
-    def _stop_mosquitto(self):
-        self.mosquitto.terminate()
-        time.sleep(1)
+    def publish(self, link: str, message: str):
+        """
+        Publish a message through this link
+        """
+        self.host_node.publish(link, message)
 
-    def _get_host_IP(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80)) #Connecting to Google and then getting whatever ip address was used
-        return s.getsockname()[0]
-
-    def _get_bot_IP(self, mac: str):
-        if (self._bot_ip == None):
-            self._bot_ip = {}
-            macs = []
-            for bot in self.bots:
-                macs.append(bot.mac)
-            command = ["sudo", "arp-scan", "--localnet"]
-            process = subprocess.Popen(command, stdout = subprocess.PIPE)
-            while True:
-                line = process.stdout.readline()
-                if not line or len(macs) == 0:
-                    break
-                for bot_mac in macs:
-                    if bot_mac in line.decode():
-                        self._bot_ip[bot_mac] = line.decode().rstrip().split()[0]
-                        macs.remove(bot_mac)
-        ip = ""
-        try:
-            ip = self._bot_ip[mac]
-            print("Bot IP:", ip)
-        except:
-            raise Exception("Bot not found on local network")
-
-        return ip
+    def publish_all(self, message: str):
+        """
+        Publish a message through all links connected to this node
+        """
+        for link in self.host_node.publishable_links:
+            self.host_node.publish(link, message)
