462af723ceadaba2910fd2056225d98656c62751
Locke Adams <lockeadams@gmail.com>
2021-02-28 21:36:26 -0500

    Updated ssh_connect.sh * Now accepts pi's label as argument instead of mac address.

--- util/ssh_connect.sh
+++ util/ssh_connect.sh
@@ -1,5 +1,14 @@
-<<<<<<< HEAD
-ssh pi@$(sudo arp-scan --local | grep $1 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
-=======
-ssh pi@$(sudo arp-scan --local | grep b8:27:eb:ef:9b:60 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
->>>>>>> dca026148281862bb0672ac0285d20a6fb50a5a8
+#!/bin/bash
+# ssh_connect.sh - script to connect to a PI given its label (A,B,C)
+# by finding its IP address. Labels are not case sensitive.
+
+declare -A mac
+mac["A"]="b8:27:eb:28:65:ca"
+mac["B"]="b8:27:eb:7b:d9:70"
+mac["C"]="b8:27:eb:ef:9b:60"
+
+pi=${1^^}
+addr=${mac[$pi]}
+
+echo Connecting to Pi Zero $pi at MAC address $addr
+ssh pi@$(sudo arp-scan --local | grep $addr | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
